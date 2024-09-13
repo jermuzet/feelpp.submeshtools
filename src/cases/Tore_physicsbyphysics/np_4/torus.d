35970a1891600ca02e2e90c351c25e0352dbf79a
jermuzet <jeremie.muz@gmail.com>
2024-09-13 09:17:56 +0000

    add Tore case

---
+++ src/cases/Tore_physicsbyphysics/np_4/torus.d
@@ -0,0 +1,18 @@
+#Power[MW]	Current[A]
+0.0018 		31000.
+#Helices	N_Elem
+0  		
+#N	R1[m]  R2[m]  HalfL[m]	Rho[Ohm.m]  Alpha[1/K]	E_Max[Pa]	K[W/(m.K)]	h[W/(m^2.K)]	<T_Water>[°C]	T_Max[°C]
+0.
+1
+# Bitter 
+#Type	R1       R2	     Z1	         Z2	         J		    Rho	   N_turns
+1       0.075      0.100   -0.025    0.025     29000000    1.72413e-08         1
+
+#Bz(0)[tesla]	Power[MW]	Bz_total(0)[tesla]
+0.41    0.0018     0.41
+
+ 
+ 
+
+MARGE DE SECURITE CONTRAINTES= 0.0 %
